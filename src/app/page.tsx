6b50e4a28723204b5206f4dd58be22bf49190e9f
NovaFrost <holyprime1985@gmail.com>
2023-09-20 20:56:18 +0700

    Updated. (Tags still un-useable)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,20 @@
 "use client"
-import Image from 'next/image'
 import Navbar from './../../components/navbar';
 import { Cards } from '../../components/cards';
 import { useEffect, useState } from "react";
 import SearchBar from '../../components/searchbar';
-import { Context } from 'urql';
+
 import React from 'react';
-import Head from 'next/head';
-import { cacheExchange, createClient, fetchExchange, gql } from '@urql/core';
-import { registerUrql } from '@urql/next/rsc';
-import { Suspense } from 'react';
-import { useQuery} from '@urql/next';
+
 import { inView, motion} from "framer-motion"
-import { useRef } from 'react'
+
 import ScrollToTop from '../../components/scrolltotop';
   
 export default function Home() {
     return (
-    
       <>
       <ScrollToTop/>
       <Navbar/>
-
-     
       <motion.div
         variants={{
           hidden: { opacity: 0, y: 75},
@@ -32,18 +24,8 @@ export default function Home() {
         animate="visible"
         transition={{duration: 1, delay:0.25}}
         >
-
-        
-       
-
-
         <Cards/>  
-        
         </motion.div>
-        
         </>
-      
-      
-   
   )
 }
