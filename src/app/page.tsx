0517cdd2ef7dac595257f3dfa4d2dcd6eb8a6b8d
NovaFrost <holyprime1985@gmail.com>
2023-09-25 12:08:10 +0700

    publish test card, test case

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,21 @@
 "use client"
 import Navbar from './../../components/navbar';
-import { Cards } from '../../components/cards';
+import { Cards } from '../../components/test';
 import { useEffect, useState } from "react";
 import SearchBar from '../../components/searchbar';
-
 import React from 'react';
-
 import { inView, motion} from "framer-motion"
-
 import ScrollToTop from '../../components/scrolltotop';
+import Footer from '../../components/footer';
   
 export default function Home() {
     return (
       <>
       <ScrollToTop/>
       <Navbar/>
+      <div className='p-1 pt-0 sm:p-3 md:p-6 lg:p-10'>
+      <SearchBar/>
+      </div>
       <motion.div
         variants={{
           hidden: { opacity: 0, y: 75},
@@ -24,8 +25,24 @@ export default function Home() {
         animate="visible"
         transition={{duration: 1, delay:0.25}}
         >
-        <Cards/>  
+        <div className='grid grid-cols-1 md:grid-cols-3 sm:grid-cols-2 lg:grid-cols-4  
+        px-2 pb-0 pt-0 gap-2 sm:px-3 md:px-4 lg:px-[80px]
+        sm:gap-3 md:gap-6 lg:gap-10'>
+        <Cards/> 
+        <Cards/> 
+        <Cards/> 
+        <Cards/> 
+        <Cards/> 
+        <Cards/> 
+        <Cards/> 
+        <Cards/> 
+        <Cards/> 
+        <Cards/> 
+        <Cards/> 
+        <Cards/>
+        </div>  
         </motion.div>
+        <Footer/>
         </>
   )
 }
